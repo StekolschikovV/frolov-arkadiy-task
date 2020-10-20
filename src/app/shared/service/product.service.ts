a14e880aa195efc5fedee81fe74d9befe953e95a
mk <v.a.stekolschikov@gmail.com>
2020-10-20 10:28:57 +0300

    init

---
+++ src/app/shared/service/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {ProductInterface} from "../Type/product.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  get() {
+
+  }
+
+  toggleFavorite(id: string) {
+
+  }
+
+}
