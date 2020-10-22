0dade19f3cc4075e07af4dd8c4a4d99038c53d9c
mk <v.a.stekolschikov@gmail.com>
2020-10-22 11:38:05 +0300

    vue complete

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,29 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <div class="row">
+      <div v-for="product of productList" :key="product.id" class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+          <img alt="Card image cap"
+               class="card-img-top"
+               src="https://www.gannett-cdn.com/-mm-/ada32a4f5055f07292db9003891f8c154f8c42b1/c=0-109-2122-1303/local/-/media/2020/08/11/USATODAY/usatsports/MotleyFool-TMOT-deeef720-rental-home.jpg">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">type: {{ product.type }}</li>
+            <li class="list-group-item">rooms: {{ product.attributes.rooms }}</li>
+            <li class="list-group-item">area: {{ product.attributes.area }} {{ product.attributes.unit }}</li>
+            <li class="list-group-item">address: {{ product.attributes.address.city }}
+              , {{ product.attributes.address.street }} {{ product.attributes.address.house }}
+              /{{ product.attributes.address.room }}
+            </li>
+            <li class="list-group-item">
+              seller: {{ product.relationships.attributes.last_name }} {{ product.relationships.attributes.first_name }}
+            </li>
+          </ul>
+          <div class="card-body">
+            <button class="btn btn-primary" type="button"
+                    v-bind:class="{'bg-success': product.isFavorite}"
+                    v-on:click="toggleFavorite(product)">
+              {{ product.isFavorite ? "Disliked" : "Like" }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
 
-export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld
+import {environment} from "./environment";
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      productList: []
+    }
+  },
+  components: {},
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      axios.get(environment.productUrl).then((e) => {
+        this.productList = this.normalizeData(e.data.response)
+      })
+    },
+    normalizeData(dataIn) {
+      let newProductList = dataIn.map((e) => {
+        e.isFavorite = false
+        return e
+      })
+      return newProductList
+    },
+    toggleFavorite(toggleProduct) {
+      let newProductList = this.productList.map((e) => {
+        if (e.id === toggleProduct.id) {
+          e.isFavorite = !e.isFavorite
+          return e
+        } else {
+          return e
+        }
+      })
+      this.productList = newProductList
+    }
   }
-});
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+
 </style>
---
+++ vue/src/environment.js
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    productUrl: "https://raw.githubusercontent.com/FrolovArkadiy/task_for_middle/master/entities.json"
+};
